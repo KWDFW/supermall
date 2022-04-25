466a4693147da6bfe68aff86413ff2e402ff92a7
KWDFW <3107861738@qq.com>
2022-04-25 09:46:16 +0800

    2轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
 </template>
 
 <script>
-export default {
-  name:'Home'
-}
+  import NavBar from '../../components/common/navbar/NavBar.vue'
+  import HomeSwiper from './childComps/HomeSwiper.vue'
+
+  import {getHomeMultidata} from '../../network/home.js'
+
+  export default {
+    name:'Home',
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    data(){
+      return{
+        banners:[],
+        recommends:[]
+      }
+    },
+    created () {
+      getHomeMultidata().then(res=>{
+        this.banners=res.data.banner.list
+        this.recommends=res.data.recommend.list
+      })
+    }
+  }
 </script>
 
 <style>
-
+  .home-nav{
+   background-color: var(--color-tint); 
+   color:white;
+  }
 </style>
