3a7e1f1dd9a8df1b9539d5301ff9902620959ddf
KWDFW <3107861738@qq.com>
2022-04-25 21:39:42 +0800

    3推荐栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,20 +2,31 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
   </div>
 </template>
 
 <script>
-  import NavBar from '../../components/common/navbar/NavBar.vue'
   import HomeSwiper from './childComps/HomeSwiper.vue'
+  import RecommendView from '@/views/home/childComps/RecommendView.vue'
+  import FeatureView from '@/views/home/childComps/FeatureView.vue'
+
+  import NavBar from '../../components/common/navbar/NavBar.vue'
+  import TabControl from '../../components/content/tabControl/TabControl.vue'
+
 
   import {getHomeMultidata} from '../../network/home.js'
 
   export default {
     name:'Home',
     components: {
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
       NavBar,
-      HomeSwiper
+      TabControl
     },
     data(){
       return{
@@ -33,8 +44,25 @@
 </script>
 
 <style>
+  #home{
+    padding-top: 44px;
+    padding-bottom: 4000px;
+    /* 暂时显示滚动条 */
+  }
+
   .home-nav{
    background-color: var(--color-tint); 
    color:white;
+
+   position: fixed;
+   left:0;
+   right: 0;
+   top: 0;
+   z-index: 9;
+  }
+
+  .tab-control{
+    position: sticky;
+    top:44px;
   }
 </style>
