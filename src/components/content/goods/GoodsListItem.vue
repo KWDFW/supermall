83e044bea89a830a16fecf3ffd50c05729f32f78
KWDFW <3107861738@qq.com>
2022-04-28 14:38:49 +0800

    5滚动条

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods-item">
-    <img :src="goodsItem.show.img" alt="">
+    <img :src="goodsItem.show.img" alt="" @load="image">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
       <span class="price">{{goodsItem.price}}</span>
@@ -23,7 +23,7 @@
   }
 </script>
 
-<style>
+<style scoped>
   .goods-item {
     padding-bottom: 40px;
     position: relative;
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" 
-      :titles="['流行','新款','精选']"
-      @tabClick="tabClick"></tab-control>
-    <goods-list :goods="showGoods"></goods-list>
+
+    <scroll class="content" 
+      ref="scroll" 
+      :probe-type="3" 
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp="loadMore">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" 
+        :titles="['流行','新款','精选']"
+        @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
@@ -19,12 +29,14 @@
   import NavBar from '../../components/common/navbar/NavBar.vue'
   import TabControl from '../../components/content/tabControl/TabControl.vue'
   import GoodsList from '../../components/content/goods/GoodsList.vue'
-
+  import Scroll from '../../components/common/scroll/Scroll.vue'
+  import BackTop from '../../components/content/backTop/BackTop.vue'
 
   import {
     getHomeMultidata,
     getHomeGoods
     } from '../../network/home.js'
+
 
   export default {
     name:'Home',
@@ -34,7 +46,9 @@
       FeatureView,
       NavBar,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll,
+      BackTop
     },
     data(){
       return{
@@ -45,7 +59,8 @@
           'new':{page:0,list:[]},
           'sell':{page:0,list:[]},
         },
-        currentType:'pop'
+        currentType:'pop',
+        isShowBackTop:false,
       }
     },
     computed: {
@@ -74,6 +89,19 @@
         }
       },
 
+      backClick(){
+        this.$refs.scroll.scrollTo(0,0)
+        // console.log(this.$refs.scroll.scroll.scrollTo(0,0,500))
+      },
+
+      contentScroll(position){
+        this.isShowBackTop=(-position.y)>1000
+      },
+
+      loadMore(){
+        this.getHomeGoods(this.currentType)
+      },
+
       //网络请求相关的方法
       getHomeMultidata(){
         getHomeMultidata().then(res=>{
@@ -87,15 +115,20 @@
           this.goods[type].list.push(...res.data.list)
           //...可以把一个数组展开成一串数，push可以推入一串数
           this.goods[type].page++
+
+          this.$refs.scroll.finishPullUp()
         })
-      }
+      },
+
     }
   }
 </script>
 
-<style>
+<style scoped>
   #home{
-    padding-top: 44px;
+    /* padding-top: 44px; */
+    height:100vh;
+    position: relative;
   }
 
   .home-nav{
@@ -110,9 +143,28 @@
   }
 
   .tab-control{
-    position: sticky;
+    /* position: sticky; */
     top:44px;
     z-index:9;
-    background-color: white;
+    /* background-color: white; */
   }
+
+  .content{
+    /* height:5355px; */
+    overflow: hidden;
+    /* margin-top: 44px; */
+
+    position: absolute;
+    top:44px;
+    bottom: 49px;
+    left:0;
+    right:0;
+    /* height:10000px;*/
+
+    /* height: calc(100% - 93px);
+    overflow: hidden;
+    margin-top: 44px; */
+  }
+
+  
 </style>
